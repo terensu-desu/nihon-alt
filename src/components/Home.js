ed63c55c9ba08f72b6af628ebb9c0b796553ae93
Terence <tmangram@gmail.com>
2017-09-10 17:25:26 +0900

    Added Articles and Uploads components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import RecentUploadsList from './RecentUploadsList'
+import ArticlesList from './ArticlesList'
 //note: for Recent Uploads section, remove bottom-margin
 
 class Home extends Component {
@@ -10,25 +12,7 @@ class Home extends Component {
             <div className="card z-depth-4">
                 <div className="card-content">
                     <h4 className="center">Recent Uploads</h4>
-                    <div className="row">
-                        <div className="col s12 center">
-                            <h5>Guess Who Mario Version! <span>by Thomas T.</span></h5>
-                            <p>Students will enjoy and master the grammar with this! <span>Level: Year 1</span></p>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col s12 center">
-                            <h5>Guess Who Gundam Version! <span>by Palmer C.</span></h5>
-                            <p>Students will enjoy and master the grammar with this! <span>Level: Year 1</span></p>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col s12 center">
-                            <h5>Guess Who Sailor Moon Version! <span>by Walt S.</span></h5>
-                            <p>Students will enjoy and master the grammar with this! <span>Level: Year 1</span></p>
-                        </div>
-                    </div>
-                    <hr/>
+                    <RecentUploadsList />
                     <div className="row">
                         <div className="col s12 center">
                             <h5>File Upload</h5>
@@ -39,80 +23,20 @@ class Home extends Component {
                 </div>
             </div>
         </div>
+
         <div className="col s12 l8">
             <div className="card z-depth-4">
                 <div className="card-content">
                     <h4 className="center">Otsukaresama News / Articles</h4>
                     <div className="row">
-                        <div className="col s12">
-                            <h5>My Transformation from ALT to Super ALT <span>by Nate G.</span></h5>
-                            <p>Lorem ipsum pimpin' sit amizzle, consectetuer adipiscing crunk. Yo sapien velizzle, doggy volutpizzle, suscipizzle crackalackin, gravida vizzle, dizzle. Pellentesque we gonna chung gizzle. Sed erizzle. Fusce izzle dolor dapibus turpizzle tempizzle rizzle. Maurizzle pellentesque nibh dawg turpis.<a className="modal-trigger" href="#modal1"> Continue reading...</a></p>
-                            <hr/>
-                        </div>
-                        <div className="col s12">
-                            <h5>Tips for New ALTs From Grizzled Veterans <span>by Rachel W.</span></h5>
-                            <p>Lorem ipsum pimpin' sit amizzle, consectetuer adipiscing crunk. Yo sapien velizzle, doggy volutpizzle, suscipizzle crackalackin, gravida vizzle, dizzle. Pellentesque we gonna chung gizzle. Sed erizzle. Fusce izzle dolor dapibus turpizzle tempizzle rizzle. Maurizzle pellentesque nibh dawg turpis.<a className="modal-trigger" href="#modal2"> Continue reading...</a></p>
-                            <hr/>
-                        </div>
-                        <div className="col s12">
-                            <h5>Saving Money in Japan - Slash Your Phone Bill <span>by Karl G.</span></h5>
-                            <p>Lorem ipsum pimpin' sit amizzle, consectetuer adipiscing crunk. Yo sapien velizzle, doggy volutpizzle, suscipizzle crackalackin, gravida vizzle, dizzle. Pellentesque we gonna chung gizzle. Sed erizzle. Fusce izzle dolor dapibus turpizzle tempizzle rizzle. Maurizzle pellentesque nibh dawg turpis.<a href="#!"> Continue reading...</a></p>
-                            <hr/>
-                        </div>
-                        <div className="col s12">
-                            <h5>Hello, World - NihonALT Has Landed <span>by Terence M.</span></h5>
-                            <p>Lorem ipsum pimpin' sit amizzle, consectetuer adipiscing crunk. Yo sapien velizzle, doggy volutpizzle, suscipizzle crackalackin, gravida vizzle, dizzle. Pellentesque we gonna chung gizzle. Sed erizzle. Fusce izzle dolor dapibus turpizzle tempizzle rizzle. Maurizzle pellentesque nibh dawg turpis.<a href="#!"> Continue reading...</a></p>
-                            <hr/>
-                        </div>
+                        <ArticlesList />
                         <p className="center">Article <a>archive</a></p>
                     </div>
                 </div>
         	</div>
         </div>
 
-        <div id="modal1" className="modal">
-            <div className="modal-content">
-                <div className="row">
-                    <div className="col s12">
-                        <div className="card">
-                            <div className="card-content">
-                                <h4 className="card-title">My Transformation from ALT to Super ALT <span>by Nate G.</span></h4>
-                                <p>Lorizzle ipsizzle dolor black tellivizzle, check it out adipiscing elit. Nullizzle bling bling velizzle, crunk volutpizzle, suscipizzle quis, gravida vizzle, arcu. Pellentesque things tortizzle. Sizzle erizzle. Shit at ma nizzle dapibus turpis fizzle i saw beyonces tizzles and my pizzle went crizzle. Maurizzle pellentesque nibh izzle you son of a bizzle. Vestibulum in break it down. Pellentesque eleifend rhoncus tellivizzle. In owned that's the shizzle platea i saw beyonces tizzles and my pizzle went crizzle. Hizzle dapibizzle. Crunk nizzle sizzle, pretium sizzle, its fo rizzle fo, eleifend vitae, nunc. Daahng dawg suscipizzle. Integer sempizzle gangster da bomb.</p>
-                                <p>Donizzle break it down shut the shizzle up dang. Phasellizzle bow wow wow elit fo shizzle mammasay mammasa mamma oo sa fo tincidunt. Cool ma nizzle nizzle. Boofron in lacus rizzle elementizzle tristique. Mofo things break it down sizzle its fo rizzle mammasay mammasa mamma oo sa ultricizzle porta. Yo mamma yippiyo shiznit, ultricizzle at, hendrerizzle brizzle, sheezy bow wow wow, check out this. Etizzle velit break yo neck, yall, bow wow wow cool, pharetra nizzle, dictizzle i saw beyonces tizzles and my pizzle went crizzle, sizzle. Sheezy neque. Bizzle shit. Izzle vitae erizzle ut libero commodo yippiyo. Fusce izzle augue da bomb nibh pot mattizzle. Phasellus shizzle my nizzle crocodizzle sapien nizzle boofron. Suspendisse black leo, fo shizzle sizzle, yippiyo izzle, brizzle nizzle, justo. Donizzle uhuh ... yih! porttitor yippiyo. Nunc boofron, ghetto nizzle ornare tempor, sapien funky fresh tincidunt yippiyo, my shizz shit pede enizzle izzle pizzle. Phasellizzle quam leo, cool izzle, tempus izzle, sempizzle izzle, pimpin'. Ut sure magna vel ipsum. Sed ante shizzlin dizzle, suscipizzle mofo, vestibulum uhuh ... yih!, rutrizzle boofron, velizzle. Mauris a yippiyo. Sed ass magna away amizzle risus iaculizzle sure.</p>
-                                <p>Etizzle tellivizzle away dang augue fo shizzle my nizzle accumsizzle. Aenean fo shizzle mah nizzle fo rizzle, mah home g-dizzle est. Vivamizzle mammasay mammasa mamma oo sa dolor, viverra dope, boofron id, ultricizzle izzle, erat. Shiz ante ipsizzle primis izzle faucibus orci i saw beyonces tizzles and my pizzle went crizzle izzle ultrices posuere Gangster; Donec dolizzle. Check out this faucibizzle. Bow wow wow that's the shizzle blandit quizzle. Fo shizzle rutrizzle pot orci. Shiz facilisizzle. Fo shizzle mah nizzle fo rizzle, mah home g-dizzle sizzle nulla, ma nizzle mah nizzle, scelerisque cool, blandizzle fo shizzle mah nizzle fo rizzle, mah home g-dizzle, magna.</p>
-                                <p>Nulla facilisi. Fizzle faucibus pharetra neque. Tellivizzle vulputate check it out izzle mauris. Cras accumsan odio things ipsizzle. CurabiNulla facilisi. Fizzle faucibus pharetra neque. Tellivizzle vulputate check it out izzle mauris. Cras accumsan odio things ipsizzle. Curabitur adipiscing rizzle vizzle ass. Crizzle fo, mi shiznit eleifend fo shizzle, dolor bizzle bibendum crunk, eu placerizzle my shizz ghetto gangster shiznit. Etizzle the bizzle, lectizzle et bling bling owned, tellizzle i saw beyonces tizzles and my pizzle went crizzle lacinia dang, a fo shizzle nulla we gonna chung izzle purus. Sure aliquet mattizzle ghetto. In its fo rizzle aliquizzle izzle. Daahng dawg leo enizzle, da bomb nec, facilisis nec, the bizzle fo shizzle, bling bling. Dang eget massa yo mamma risizzle blandizzle dictizzle. Crunk ullamcorpizzle turpizzle shizzlin dizzle sem shizzlin dizzle yo mamma. Shizzlin dizzle fizzle gizzle. Maecenas lacinia. Fizzle fermentizzle fizzle enim. Pellentesque sizzle. Lorem ipsizzle crunk sizzle amizzle, brizzle pimpin' elizzle. Maurizzle urna arcu, convallis egizzle, sagittizzle hizzle, break yo neck, yall ma nizzle, neque. Lorizzle ipsizzle gangsta yo mamma amet, consectetuer things elizzle.tur adipiscing rizzle vizzle ass. Crizzle fo, mi shiznit eleifend fo shizzle, dolor bizzle bibendum crunk, eu placerizzle my shizz ghetto gangster shiznit. Etizzle the bizzle, lectizzle et bling bling owned, tellizzle i saw beyonces tizzles and my pizzle went crizzle lacinia dang, a fo shizzle nulla we gonna chung izzle purus. Sure aliquet mattizzle ghetto. In its fo rizzle aliquizzle izzle. Daahng dawg leo enizzle, da bomb nec, facilisis nec, the bizzle fo shizzle, bling bling. Dang eget massa yo mamma risizzle blandizzle dictizzle. Crunk ullamcorpizzle turpizzle shizzlin dizzle sem shizzlin dizzle yo mamma. Shizzlin dizzle fizzle gizzle. Maecenas lacinia. Fizzle fermentizzle fizzle enim. Pellentesque sizzle. Lorem ipsizzle crunk sizzle amizzle, brizzle pimpin' elizzle. Maurizzle urna arcu, convallis egizzle, sagittizzle hizzle, break yo neck, yall ma nizzle, neque. Lorizzle ipsizzle gangsta yo mamma amet, consectetuer things elizzle.</p>
-                                <a className="card-title btn activator">View comments</a>
-                            </div>
-                            <div className="card-reveal">
-                                <span>Comment and Discuss</span>
-                                <div className="row">
-                                    <div className="col s12">
-                                        <p>CANCER COMMENTS HERE</p>
-                                        <a className="card-title btn">Close comments</a>
-                                    </div>
-                                    <form className="col s12">
-                                        <div className="row">
-                                            <div className="input-field col s12">
-                                                <textarea id="textarea1" className="materialize-textarea"></textarea>
-                                                <label for="textarea1">Textarea</label>
-                                                <a className="btn">Submit comment</a>
-                                                <p>Comments subject to approval.</p>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="modal-footer">
-                <div className="row center">
-                    <h5>Share this article</h5>
-                    <p>Twitter -- Facebook -- Reddit</p>
-                </div>
-            </div>
-        </div>
+        
 
     </div>
     )
